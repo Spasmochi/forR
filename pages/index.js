f5faea0f5f16dc0aaa83cdf8184da82bd19215a6
Spas <12554483+Spasmochi@users.noreply.github.com>
2020-10-08 17:21:25 +1100

    cleaned up prismic stuff, wh, added components

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import Head from "next/head";
 import { Container } from "../layout/Container";
+import { Client } from "../utils/prismic";
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return (
     <Container>
       <Head>
-        <title>Case Study | Marley Spoon</title>
+        <title>Case Study | Marley Spoon </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </Container>
   );
 }
+
+export const getStaticProps = async () => {
+  const doc = await Client.query("");
+  return { props: { doc } };
+};
--- utils/prismic.js
+++ utils/prismic.js
@@ -2,7 +2,7 @@ import Prismic from "prismic-javascript";
 
 const REPO = "funpages";
 
-export const URL = `https://${REPO}.cdn.prismatic.io/api/vw`;
+export const URL = `https://${REPO}.cdn.prismic.io/api/v2`;
 export const TOKEN = process.env.API_TOKEN;
 
 export const Client = Prismic.client(URL, { TOKEN });
